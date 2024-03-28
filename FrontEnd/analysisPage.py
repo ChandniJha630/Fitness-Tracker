8b98e6104c4d4f0ddd8de46771cbb3999e222f54
CHANDNI JHA <96429544+ChandniJha630@users.noreply.github.com>
2024-03-28 14:48:32 +0530

    Update analysisPage.py

--- FrontEnd/analysisPage.py
+++ FrontEnd/analysisPage.py
@@ -301,7 +301,7 @@ def show_charts(user_id, conn):
     granularity = st.select_slider('Select Granularity', options =['Daily','Weekly','Monthly'],value='Daily')
 #--------------------------------------------------------------------------------------------------------
     st.divider()
-    st.title("Water Intake Tracker")
+    
     # Based on the selected granularity, display the appropriate chart
     if granularity == 'Weekly':
         st.write("Weekly Chart")
@@ -318,153 +318,162 @@ def show_charts(user_id, conn):
     else:
         val='week_number'
     water_df = fetch_water_intake(user_id, cur,weight_kg,granularity)
-    st.write(water_df)
-    plot_data(water_df, "Water Intake", "Date", "Water Intake (ml)", 600,weight_kg*30, granularity)
+    
+    if not water_df.empty:
+        st.title("Water Intake Tracker")
+        st.write(water_df)
+        plot_data(water_df, "Water Intake", "Date", "Water Intake (ml)", 600,weight_kg*30, granularity)
 
-    posW=0
-    negW=0
-    for each in water_df['Suggestion']:
-        if each == "Great job! You are perfectly hydrated":
-            posW+=1
+        posW=0
+        negW=0
+        for each in water_df['Suggestion']:
+            if each == "Great job! You are perfectly hydrated":
+                posW+=1
+            else:
+                negW+=1
+        # Create labels and corresponding values for the pie chart
+        labels = ['Hydrated','Dehydrated']
+        values = [posW,negW]
+
+        # Define colors for each sleep stage
+        colors = ['lightblue', 'lightgreen', 'lightcoral']
+        from plotly.colors import sequential
+
+        colorscale = sequential.Viridis
+        # Create the pie chart
+        fig = go.Figure(data=[go.Pie(labels=labels, values=values, hole=0.3 ,marker_colors=colorscale,
+                        textinfo='value+percent', textposition='inside',
+                        textfont=dict(size=20,color='white'))])
+
+
+        # Update layout properties
+        fig.update_layout(
+            title="Hydration Level",
+        )
+
+        # Display the plot
+        st.plotly_chart(fig)
+        msgW=''
+        if posW<negW:
+            msgW='''Grow an habbit of drinking more water. You are mostly dehydrated !!. Staying hydrated is good for your
+                    health, it gives you glowing skin and does purification and detoxification of blood'''
         else:
-            negW+=1
-      # Create labels and corresponding values for the pie chart
-    labels = ['Hydrated','Dehydrated']
-    values = [posW,negW]
-
-    # Define colors for each sleep stage
-    colors = ['lightblue', 'lightgreen', 'lightcoral']
-    from plotly.colors import sequential
-
-    colorscale = sequential.Viridis
-    # Create the pie chart
-    fig = go.Figure(data=[go.Pie(labels=labels, values=values, hole=0.3 ,marker_colors=colorscale,
-                    textinfo='value+percent', textposition='inside',
-                    textfont=dict(size=20,color='white'))])
-
-
-    # Update layout properties
-    fig.update_layout(
-        title="Hydration Level",
-    )
-
-    # Display the plot
-    st.plotly_chart(fig)
-    msgW=''
-    if posW<negW:
-        msgW='''Grow an habbit of drinking more water. You are mostly dehydrated !!. Staying hydrated is good for your
-                 health, it gives you glowing skin and does purification and detoxification of blood'''
-    else:
-        msgW='''Great Job Dude !! A healthy person is basic building block of a healthy society'''
-    st.write(f"<p style='{info_style}'>{msgW}</p>", unsafe_allow_html=True)
+            msgW='''Great Job Dude !! A healthy person is basic building block of a healthy society'''
+        st.write(f"<p style='{info_style}'>{msgW}</p>", unsafe_allow_html=True)
     
 #-------------------------------------------------------------------------------------------------------
     st.divider()
-    st.title("Calorie Intake") 
+    
     food_df = fetch_food_intake(user_id, cur,granularity)
-    col1,col2=st.columns([1,1])
-    with col2:
-        st.write(food_df)
-    with col1:
-        plot_data(food_df, "Calories Intake", "Date", "Calories",300,0, granularity)
+    if not food_df.empty:
+        st.title("Calorie Intake") 
+        col1,col2=st.columns([1,1])
+        with col2:
+            st.write(food_df)
+        with col1:
+            plot_data(food_df, "Calories Intake", "Date", "Calories",300,0, granularity)
 
 #---------------------------------------------------------------------------------------------------------
-    st.title("Calorie Burnt")
+   
     calories_df = fetch_calories(user_id, cur,granularity)
-    col1,col2=st.columns([1,1])
-    with col2:
-        st.write(calories_df)
-    with col1:
-        plot_data(calories_df, "Calories Burnt", "Date", "Calories",300,0, granularity)
+    if not calories_df.empty:
+        st.title("Calorie Burnt")
+        col1,col2=st.columns([1,1])
+        with col2:
+            st.write(calories_df)
+        with col1:
+            plot_data(calories_df, "Calories Burnt", "Date", "Calories",300,0, granularity)
 #-----------------------------------------------------------------------------------------------------
-    st.title("Sleep Analysis")
+    
     sleep_df = fetch_sleep_time(user_id, cur,granularity)
-    st.write(sleep_df)
-    if granularity=="Monthly":
-        sd=240
-    elif granularity=="Daily":
-        sd=8
-    else:
-        sd=56  
-    col1,col2=st.columns([1,1])
-    with col1:
-    # Create a Plotly figure
-        fig = go.Figure()
-        # Add a line trace for the daily sleep duration
-        fig.add_trace(go.Scatter(x=sleep_df[f"{val}"], y=sleep_df['total_data'], mode='lines+markers', name='Sleep Duration'))
+    if not sleep_df.empty:
+        st.title("Sleep Analysis")
+        st.write(sleep_df)
+        if granularity=="Monthly":
+            sd=240
+        elif granularity=="Daily":
+            sd=8
+        else:
+            sd=56  
+        col1,col2=st.columns([1,1])
+        with col1:
+        # Create a Plotly figure
+            fig = go.Figure()
+            # Add a line trace for the daily sleep duration
+            fig.add_trace(go.Scatter(x=sleep_df[f"{val}"], y=sleep_df['total_data'], mode='lines+markers', name='Sleep Duration'))
+            # Update layout properties
+            fig.update_layout(
+                title="Sleep Duration",
+                yaxis_title="Total Sleep (hours)",
+                xaxis_tickformat='%d-%b',  # Customize x-axis tick labels to show only date and month
+                xaxis_tickangle=-45,  # Rotate x-axis tick labels
+                xaxis_showgrid=True,  # Add gridlines
+                yaxis_showgrid=True,
+                width=300
+            )
+
+            fig.add_hline(y=sd, line_dash="dash", line_color="green", annotation_text="Target", annotation_position="bottom right")
+            # Display the plot
+            st.plotly_chart(fig)
+        with col2:
+            fig1 = go.Figure()
+            # Add a line trace for the daily sleep duration
+            fig1.add_trace(go.Scatter(x=sleep_df[f"{val}"], y=sleep_df['index'], mode='lines+markers', name='Sleep Duration'))
+            # Update layout properties
+            fig1.update_layout(
+                title="Sleep Index",
+                yaxis_title="index",
+                xaxis_tickformat='%d-%b',  # Customize x-axis tick labels to show only date and month
+                xaxis_tickangle=-45,  # Rotate x-axis tick labelsr
+                xaxis_showgrid=True,  # Add gridlines
+                yaxis_showgrid=True,
+                width=300
+            )
+
+            # Add a horizontal line at y=3.7
+            fig1.add_hline(y=0.37, line_dash="dash", line_color="green", annotation_text="Target", annotation_position="bottom right")
+
+            # Display the plot
+            st.plotly_chart(fig1)
+
+        # Calculate total sleep duration for each sleep stage
+        light_sleep_total = sleep_df['light_sleep'].sum()
+        deep_sleep_total = sleep_df['deep_sleep'].sum()
+        rem_sleep_total = sleep_df['rem_sleep'].sum()
+
+        # Create labels and corresponding values for the pie chart
+        labels = ['Light Sleep', 'Deep Sleep', 'REM Sleep']
+        values = [light_sleep_total, deep_sleep_total, rem_sleep_total]
+
+        # Define colors for each sleep stage
+        colors = ['lightblue', 'lightgreen', 'lightcoral']
+        from plotly.colors import sequential
+
+        colorscale = sequential.Viridis
+        # Create the pie chart
+        fig = go.Figure(data=[go.Pie(labels=labels, values=values, hole=0.3 ,marker_colors=colorscale,
+                        textinfo='value+percent', textposition='inside',
+                        textfont=dict(size=20,color='white'))])
+
+
         # Update layout properties
         fig.update_layout(
-            title="Sleep Duration",
-            yaxis_title="Total Sleep (hours)",
-            xaxis_tickformat='%d-%b',  # Customize x-axis tick labels to show only date and month
-            xaxis_tickangle=-45,  # Rotate x-axis tick labels
-            xaxis_showgrid=True,  # Add gridlines
-            yaxis_showgrid=True,
-            width=300
+            title="Sleep Distribution",
         )
-
-        fig.add_hline(y=sd, line_dash="dash", line_color="green", annotation_text="Target", annotation_position="bottom right")
-         # Display the plot
-        st.plotly_chart(fig)
-    with col2:
-        fig1 = go.Figure()
-        # Add a line trace for the daily sleep duration
-        fig1.add_trace(go.Scatter(x=sleep_df[f"{val}"], y=sleep_df['index'], mode='lines+markers', name='Sleep Duration'))
-        # Update layout properties
-        fig1.update_layout(
-            title="Sleep Index",
-            yaxis_title="index",
-            xaxis_tickformat='%d-%b',  # Customize x-axis tick labels to show only date and month
-            xaxis_tickangle=-45,  # Rotate x-axis tick labelsr
-            xaxis_showgrid=True,  # Add gridlines
-            yaxis_showgrid=True,
-            width=300
-        )
-
-        # Add a horizontal line at y=3.7
-        fig1.add_hline(y=0.37, line_dash="dash", line_color="green", annotation_text="Target", annotation_position="bottom right")
 
         # Display the plot
-        st.plotly_chart(fig1)
-
-    # Calculate total sleep duration for each sleep stage
-    light_sleep_total = sleep_df['light_sleep'].sum()
-    deep_sleep_total = sleep_df['deep_sleep'].sum()
-    rem_sleep_total = sleep_df['rem_sleep'].sum()
-
-    # Create labels and corresponding values for the pie chart
-    labels = ['Light Sleep', 'Deep Sleep', 'REM Sleep']
-    values = [light_sleep_total, deep_sleep_total, rem_sleep_total]
-
-    # Define colors for each sleep stage
-    colors = ['lightblue', 'lightgreen', 'lightcoral']
-    from plotly.colors import sequential
-
-    colorscale = sequential.Viridis
-    # Create the pie chart
-    fig = go.Figure(data=[go.Pie(labels=labels, values=values, hole=0.3 ,marker_colors=colorscale,
-                    textinfo='value+percent', textposition='inside',
-                    textfont=dict(size=20,color='white'))])
-
-
-    # Update layout properties
-    fig.update_layout(
-        title="Sleep Distribution",
-    )
-
-    # Display the plot
-    st.plotly_chart(fig)
-    posS=0
-    negS=0
-    for each in sleep_df['index']:
-        if each>=0.37 and each<=0.38:
-            posS+=1
+        st.plotly_chart(fig)
+        posS=0
+        negS=0
+        for each in sleep_df['index']:
+            if each>=0.37 and each<=0.38:
+                posS+=1
+            else:
+                negS+=1
+        messS=''
+        if posS>negS:
+            messS='It is good to see that you had perfect sleep'
         else:
-            negS+=1
-    messS=''
-    if posS>negS:
-        messS='It is good to see that you had perfect sleep'
-    else:
-        messS='You donot care about yourself, Have a good sleep. Ideally you should sleep 8 hours a days 4 hour light sleep, 2 hour deep and 2 hour rem sleep'
-    st.write(f"<p style='{info_style}'>{messS}</p>", unsafe_allow_html=True)
+            messS='You donot care about yourself, Have a good sleep. Ideally you should sleep 8 hours a days 4 hour light sleep, 2 hour deep and 2 hour rem sleep'
+        st.write(f"<p style='{info_style}'>{messS}</p>", unsafe_allow_html=True)
     cur.close()
